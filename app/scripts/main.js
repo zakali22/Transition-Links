5d00f3bda9f919e4a3484dd34490c8a922746d61
Zakeria Ali <zak.fined@gmail.com>
2017-07-30 03:04:34 +0100

    Add the hovering functionality based on size

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+  var links = $('li');
+  links.on('mouseenter', function(event) {
+    event.preventDefault();
+    /* Act on the event */
+    var linkCoord = event.target.getBoundingClientRect();
+    console.log(linkCoord);
+    $('.highlight').css({
+      'width': linkCoord.width + 'px',
+      'height': linkCoord.height + 'px',
+      'transform': 'translate(' + linkCoord.left + 'px, ' + linkCoord.top + 'px)'
+    });
+  });
+});
